c1a60678b7f4b682b0d0e149ae5480f157cc701f
Sebastian Sauer <ssauer@posteo.de>
2021-06-20 16:44:14 +0200

    update

--- run-parse-reviews.R
+++ run-parse-reviews.R
@@ -8,8 +8,16 @@ files_to_source_w_path <- paste0("funs/", files_to_source)
 #files_to_source_w_path
 sapply(files_to_source_w_path, source)
 
-dois_to_be_parsed <- init()
+init_flog()
 
-flog.trace("Sourcing funs.")
+dois_to_be_parsed <- init_dois_to_be_extracted()
+dois_to_be_parsed 
 
-reviews <- parse_dois(dois_to_be_parsed)
+
+undebug(parse_dois)
+reviews_jh4 <- parse_dois(dois_to_be_parsed)
+
+
+# debug(parse_review)
+# 
+# parse_review(dois_to_be_parsed[[1]])
