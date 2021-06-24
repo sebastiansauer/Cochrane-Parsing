271f8629004f2608bc74e4b275a0089a467c71d2
Sebastian Sauer <ssauer@posteo.de>
2021-06-24 09:09:28 +0200

    add "null" as feature for `and_at_doi`

--- funs/init_dois_to_be_extracted.R
+++ funs/init_dois_to_be_extracted.R
@@ -1,26 +1,75 @@
 init_dois_to_be_extracted <- function(){
   
+  
   flog.trace("Reading dois file.")
+  flog.info("init_dois_to_be_extracted", name = "funlog")
+  
+
+  flog.trace("Reading `config.yaml`.")
+  config <- read_yaml("config.yaml")
+  
+  verbose <- config$verbose
+  if (verbose) print("`config.yaml` has been red.")
+  
+  if (verbose) print("Reading dois file.")
+  
   dois_list <- readxl::read_xlsx(config$dois_file)
   
+  reviewer_list <-
+    dois_list %>% 
+    count(reviewer) %>% 
+    pull(reviewer) %>% 
+    tolower()
+  
+  flog.trace(paste0("This reviewer list was found in `dois_file: ", reviewer_list))
+  
+  if (!(tolower(config$reviewer) %in% reviewer_list)) {
+    stop("Reviewer of config file not find in dois data file.")
+    flog.error("Reviewer of config file not find in dois data file.")
+  }
+    
+  
   if (!dir.exists(config$output_path)) {
-    flog.trace("Creating directory as to output_path")
+    flog.trace("Creating directory as to output_path.")
     dir.create(config$output_path)
+    if (verbose) print("Creating directory as to output_path.")    
   }
   
-  
+  if (verbose) writeLines(paste0("Selected reviewer is: ",config$reviewer))
+  flog.trace(paste0("Selected reviewer is: ", config$reviewer))
   flog.trace("Filtering dois for selected reviewer.")
-  dois_of_selected_reviewer <- 
+  if (verbose) print("Filtering dois for selected reviewer.")    
+ 
+  dois_list_selected_reviewer <- 
     dois_list %>% 
-    dplyr::filter(tolower(reviewer) == config$reviewer) %>% 
-    dplyr::slice(config$start_at_doi:config$end_at_doi) %>% 
-    dplyr::pull(url)
+    dplyr::filter(tolower(reviewer) == config$reviewer)
   
-  flog.trace(paste0("Selected reviewer is: ",config$reviewer))
-  flog.trace(paste0("First doi to be extracted is: ", 
-                    dois_of_selected_reviewer[[1]]))
+
+  
+  if (is.null(config$start_at_doi)) config$start_at_doi <- 1
+  if (is.null(config$end_at_doi)) config$end_at_doi <- nrow(dois_list_selected_reviewer)
+  
+   dois_list_selected_reviewer <- 
+      dois_list_selected_reviewer %>% 
+      dplyr::slice(config$start_at_doi:config$end_at_doi)
+ 
+  dois_of_selected_reviewer <-   
+    dois_list_selected_reviewer %>% 
+    dplyr::pull(url)
+
   flog.trace(paste0("Number of dois to be extracted is: ", 
                     length(dois_of_selected_reviewer)))
+  
+  flog.trace(paste0("First doi to be extracted is: ", 
+                    dois_of_selected_reviewer[[1]]))
+
+  if (verbose) writeLines(paste0("First doi to be extracted is: ", 
+                                 dois_of_selected_reviewer[[1]]))
+  if (verbose) writeLines(paste0("Number of dois to be extracted is: ", 
+                                 length(dois_of_selected_reviewer)))
+  
+  
+  
   return(dois_of_selected_reviewer)
   
 }
--- funs/init_flog.R
+++ funs/init_flog.R
@@ -3,18 +3,15 @@ init_flog <- function() {
 
   flog.appender(appender.file('parse-dois.log'))
   flog.info("Starting init.")
-  flog.trace("libs loaded.")
+ 
   
   flog.appender(appender.file("funs-called.log"), 
                 name = "funlog")
-  flog.info("init_flog", name = "funlog")
   
-  
-  flog.trace("Reading config.caml")
   stopifnot(file.exists("config.yaml"))
-  flog.info("Object 'config' defined as global variable")
   
   config <<- read_yaml("config.yaml")
+  if (config$verbose) print("Config file has been read.")
   
   flog.threshold(config$flog_threshold)
   if (config$rm_old_logfile) {
@@ -23,6 +20,13 @@ init_flog <- function() {
     flog.info("Removing existing log file. Starting fresh log file.")
   }
   
+  flog.trace("Log files declared.")
+  flog.info("init_flog", name = "funlog")
+  flog.trace("Reading config.caml")
+  flog.info("Object 'config' defined as global variable")
+  
+  if (config$verbose) print("Config files have been declared.")
+  if (config$verbose) print("`init_flog` has been run.")  
 
   
 }
